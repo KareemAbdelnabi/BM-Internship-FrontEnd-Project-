a6dcb1ad519dd257a1584c85ae77e4156f5ae21b
Mahmoud Ashraf <mmehany5@gmail.com>
2024-09-05 02:53:49 +0300

    i made most of the shared components which is navbar , footer , mobileapplication components , still i have to work on the responsiviness of the webiste . Also i made some components to work with in the navbar wich help, home, money transferand my account

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// Import your components here
+import { HomeComponent } from './home/home.component';
+import { MoneyTransferComponent } from './money-transfer/money-transfer.component';
+import { MyAccountComponent } from './my-account/my-account.component';
+import { HelpComponent } from './help/help.component';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component';
+// import { NotFoundComponent } from './not-found/not-found.component'; // Assuming you have a 404 component
+
+
+export const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'money-transfer', component: MoneyTransferComponent },
+  { path: 'my-account', component: MyAccountComponent },
+  { path: 'help', component: HelpComponent }
+  //   { path: 'login', component: LoginComponent },
+  //   { path: 'register', component: RegisterComponent },
+
+  // Add more routes as needed for other parts of your application
+
+  //   { path: '**', component: NotFoundComponent }, // Wildcard route for handling 404s
+];
