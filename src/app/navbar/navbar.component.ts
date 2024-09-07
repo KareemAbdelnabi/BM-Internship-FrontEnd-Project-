31101f712069fe04944cb78cb6184184be6069ef
Mahmoud Ashraf <mmehany5@gmail.com>
2024-09-07 16:21:36 +0300

    finished ui but still want to coonect to the backend

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,13 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { CommonModule, NgIf } from '@angular/common';
+import { isPlatformBrowser } from '@angular/common';
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, NgIf],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  tokenExists: boolean = false;
+  showDropdown: boolean = false;
 
+  constructor(private router: Router, @Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    this.checkToken();
+
+    this.router.events.subscribe(() => {
+      this.checkToken();
+    });
+  }
+
+  checkToken() {
+    if (isPlatformBrowser(this.platformId)) {
+      const token = localStorage.getItem('authToken');
+      this.tokenExists = !!token;
+
+      if (!this.tokenExists) {
+        this.showDropdown = false;
+      }
+    }
+  }
+
+  toggleDropdown() {
+    if (this.tokenExists) {
+      this.showDropdown = !this.showDropdown;
+    }
+  }
+
+  logout() {
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem('authToken'); 
+    }
+    this.router.navigate(['/login']); 
+    this.showDropdown = false; 
+  }
 }
