33fe2a2abb44f320464d6eb583eed0449da7ce48
Mahmoud Ashraf <mmehany5@gmail.com>
2024-09-09 14:42:37 +0300

    add some pic

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -50,3 +50,61 @@ export class NavbarComponent implements OnInit {
     this.showDropdown = false; 
   }
 }
+
+// import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+// import { Router, RouterLink } from '@angular/router';
+// import { CommonModule, NgIf } from '@angular/common';
+// import { isPlatformBrowser } from '@angular/common';
+// import { AuthService } from '../services/auth.service'; // Import the AuthService
+
+// @Component({
+//   selector: 'app-navbar',
+//   standalone: true,
+//   imports: [RouterLink, CommonModule, NgIf],
+//   templateUrl: './navbar.component.html',
+//   styleUrls: ['./navbar.component.scss']
+// })
+// export class NavbarComponent implements OnInit {
+//   tokenExists: boolean = false;
+//   showDropdown: boolean = false;
+//   username: string | null = null; // Variable to hold the username
+
+//   constructor(
+//     private router: Router,
+//     private authService: AuthService, // Inject the AuthService
+//     @Inject(PLATFORM_ID) private platformId: Object
+//   ) {}
+
+//   ngOnInit() {
+//     this.checkToken();
+
+//     this.router.events.subscribe(() => {
+//       this.checkToken();
+//     });
+//   }
+
+//   checkToken() {
+//     if (isPlatformBrowser(this.platformId)) {
+//       const token = this.authService.getToken();
+//       this.tokenExists = !!token;
+
+//       if (this.tokenExists) {
+//         this.username = this.authService.getUsername(); // Get username if token exists
+//       } else {
+//         this.showDropdown = false;
+//       }
+//     }
+//   }
+
+//   toggleDropdown() {
+//     if (this.tokenExists) {
+//       this.showDropdown = !this.showDropdown;
+//     }
+//   }
+
+//   logout() {
+//     this.authService.logout();
+//     this.router.navigate(['/login']);
+//     this.showDropdown = false;
+//   }
+// }
