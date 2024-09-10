0f833cd42247a42c63750611d381babc48321015
KareemAbdelnabi <97126849+KareemAbdelnabi@users.noreply.github.com>
2024-09-11 00:30:52 +0300

    user story 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
 import { MobileappsharedComponent } from './mobileappshared/mobileappshared.component';
 import { LoginComponent } from './auth/login/login.component';
+import { InactivityService } from './inactivity.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet ,NavbarComponent,FooterComponent,MobileappsharedComponent,LoginComponent], 
+  imports: [RouterOutlet ,NavbarComponent,FooterComponent,MobileappsharedComponent,LoginComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  constructor(private inactivityService: InactivityService) {}
   title = 'Speedo-Transfer';
 }
