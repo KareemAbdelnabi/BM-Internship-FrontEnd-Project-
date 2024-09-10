971931e2fa1f12badbcb3f7f2dadb53541fa3690
KareemAbdelnabi <97126849+KareemAbdelnabi@users.noreply.github.com>
2024-09-11 01:26:19 +0300

    integrating with backend

--- src/app/components/user-profile/change-password/change-password.component.ts
+++ src/app/components/user-profile/change-password/change-password.component.ts
@@ -1,5 +1,6 @@
-import { Component,OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -7,12 +8,12 @@ import { ReactiveFormsModule } from '@angular/forms';
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './change-password.component.html',
-  styleUrl: './change-password.component.scss'
+  styleUrls: ['./change-password.component.scss']
 })
 export class ChangePasswordComponent implements OnInit {
   passwordForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) {
     this.passwordForm = this.formBuilder.group({
       currentPassword: ['', Validators.required],
       newPassword: ['', Validators.required]
@@ -23,8 +24,38 @@ export class ChangePasswordComponent implements OnInit {
 
   onSubmit() {
     if (this.passwordForm.valid) {
-      console.log(this.passwordForm.value);
-      // Handle password change logic here
+      // Retrieve id from local storage
+      const id = localStorage.getItem('id');
+
+      // Retrieve token from session storage
+      const token = sessionStorage.getItem('token');
+
+      if (id && token) {
+        // Define the URL and headers
+        const url = `https://moneytransferapplication-production.up.railway.app/users/${id}/changepassword`;
+        const headers = new HttpHeaders({
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        });
+
+        // Prepare the body for the POST request
+        const body = {
+          currentPassword: this.passwordForm.value.currentPassword,
+          newPassword: this.passwordForm.value.newPassword
+        };
+
+        // Send POST request
+        this.http.put(url, body, { headers }).subscribe(
+          response => {
+            console.log('Password changed successfully:', response);
+          },
+          error => {
+            console.error('Error changing password:', error);
+          }
+        );
+      } else {
+        console.error('ID or token not found');
+      }
     }
   }
 }
