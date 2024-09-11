f1c3403fcfe8576c58abb92e9b55c058c747258e
KareemAbdelnabi <97126849+KareemAbdelnabi@users.noreply.github.com>
2024-09-11 22:01:28 +0300

    changing the background color of the selected side

--- src/app/components/user-profile/user-profile.component.ts
+++ src/app/components/user-profile/user-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
 import { MyProfileComponent } from './my-profile/my-profile.component';
 import { PaymentHistoryComponent } from './payment-history/payment-history.component';
@@ -7,13 +8,34 @@ import { ChangePasswordComponent } from './change-password/change-password.compo
 import { NavbarComponent } from '../../navbar/navbar.component';
 import { FooterComponent } from '../../footer/footer.component';
 import { MobileappsharedComponent } from '../../mobileappshared/mobileappshared.component';
+
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [RouterOutlet, MyProfileComponent, PaymentHistoryComponent, SettingsComponent, ChangePasswordComponent, NavbarComponent, FooterComponent, MobileappsharedComponent],
+  imports: [
+    RouterOutlet,
+    MyProfileComponent,
+    PaymentHistoryComponent,
+    SettingsComponent,
+    ChangePasswordComponent,
+    NavbarComponent,
+    FooterComponent,
+    MobileappsharedComponent
+  ],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.scss'
 })
-export class UserProfileComponent {
+export class UserProfileComponent implements OnInit {
 
+  currentUrl: string = '';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.currentUrl = this.router.url;
+  }
+
+  isActive(link: string): boolean {
+    return this.currentUrl === link;
+  }
 }
